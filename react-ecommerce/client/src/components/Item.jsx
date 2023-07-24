bfcaa404c086e0eafb89a9db4694e2d060efb19b
Honeybadger <rajimoshood200482@gmail.com>
2023-07-24 10:31:49 +0100

    updated currency to naira

--- react-ecommerce/client/src/components/Item.jsx
+++ react-ecommerce/client/src/components/Item.jsx
@@ -77,7 +77,7 @@ const Item = ({ item, width }) => {
             .replace(/^./, (str) => str.toUpperCase())}
         </Typography>
         <Typography>{name}</Typography>
-        <Typography fontWeight="bold">${price}</Typography>
+        <Typography fontWeight="bold">₦{price}</Typography>
       </Box>
     </Box>
   );
